7ee92b387f8c006859ccc9a0168f576505480e14
Michal Kolodziejczyk <miko@wp.pl>
2011-09-02 02:10:35 +0200

    Adding rotation and animation

--- TexturedPolygon/main.lua
+++ TexturedPolygon/main.lua
@@ -11,7 +11,7 @@ function texturize(target, texture)
   tx, ty=texture:getWidth(), texture:getHeight()
   local function placeTexture(x, y, r, g, b, a)
     if r+g+b==0 then -- black
-      return 0,0,0,128
+      return 0,0,0,0
     else -- non-black, needs a texture
       return texture:getPixel(x%tx, y%ty)
     end
@@ -44,12 +44,22 @@ function makePolygon(V, texture)
 end
 
 function love.load()
-  polygon=makePolygon({0,0, 100,0, 200,100, 100,400, 0,300, -100,200, 0,0}, 'texture.png')
-  X, Y=100,100
+  local vertices={0,0, 100,0, 200,100, 100,400, 0,300, -100,200, 0,0}
+  local textures={'face-crying.png', 'face-grin.png', 'face-plain.png', 'face-sad.png', 'face-smile-big.png', 'face-smile.png', 'face-surprise.png', 'face-wink.png'}
+  Polygons={}
+  for k,v in ipairs(textures) do
+    Polygons[k]=makePolygon(vertices, v)
+  end
+  X, Y, r=200,200,0
 end
 
 function love.draw()
-  love.graphics.draw(polygon, X, Y)
+  local idx=math.floor(5*r%#Polygons)+1
+  love.graphics.draw(Polygons[1], X-5, Y-5, 0, 1, 1, X, Y)
+  love.graphics.draw(Polygons[idx], X, Y, r, 1, 1, X, Y)
 end
 
+function love.update(dt)
+  r=r+dt
+end
 
